8d47b1772c525ac864c82a158ca949a632e1517b
Shiwank <shuwankunap69@gmail.com>
2025-03-03 19:09:12 +0530

    Added new lines

--- Projects/passmanager.py
+++ Projects/passmanager.py
@@ -1,2 +1,22 @@
 pwd = input("Enter the master password: ")
 
+def view():
+    pass
+
+def add():
+    pass
+
+mode = input("Would like to add new password or view existing password? (add/view) or press Q to quit: ").lower()
+
+while True:
+    if mode == "q":
+        break
+
+    if mode == "add":
+        add()
+    elif mode == "view":
+        view()
+    else:
+        print("Invalid mode. Please enter 'add', 'view', or q to quit.")
+        continue
+
